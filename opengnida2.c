672c8375aa715dca5b4935ebdf75c2f6c19b934e
DaniilAev <daniilaev415@gmail.com>
2026-09-13 21:57:41 +0300

    feat: update of CLI functional

--- opengnida2.c
+++ opengnida2.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-char bad_udage_msg[] = "Bad usage: use the \"opengnida --confirm [false\\true]\"";
+#include <string.h>
+
+char bad_usage_err[] = "Bad usage: use the \"opengnida2 --confirm [false\\true]\"";
+char cancelled[] = "Cancelled.";
 int main(int argc, char** argv){
     if(argc < 2){
-        fprintf(stderr, "%s", bad_udage_msg);
+        fprintf(stderr, "%s", bad_usage_err);
         exit(1);
     }
+    if (strcmp(argv[1], "--confirm")){
+        fprintf(stderr, "%s", bad_usage_err);
+        exit(1);        
+    }
+    else{
+        if (!strcmp(argv[2], "false")){
+            fprintf(stdout, "%s", cancelled);
+            exit(0);
+        }
+        if (!strcmp(argv[2], "true")){
+            goto start;
+        }
+        fprintf(stderr, "%s", bad_usage_err);
+        exit(1);
+    }
+    start:
     return 0;
 }
